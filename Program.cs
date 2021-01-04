9792e4f4c8373f96ef78c916162f896f0adde765
João Alfredo <jalfredof83@gmail.com>
2021-01-04 09:09:42 -0300

    Mensagem de bom dia foi colocada

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace projeto_teste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como vc está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.Read();
         }
